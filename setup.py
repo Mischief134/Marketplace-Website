b648085187349d1139adee6ffec55f50a52d8c84
Nuri <nuri.amiraslan@mail.mcgill.ca>
2020-03-11 20:24:00 -0400

    Added user kinda

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Django'],  # Optional
+    install_requires=['Django','Pillow'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
