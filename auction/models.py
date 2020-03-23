3028f464e28cdc458790772f4a8dfdcecaa83e38
SC Haw <haw@neverguess.ca>
2020-03-22 21:55:34 -0400

    data should show now

--- auction/models.py
+++ auction/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+
 class Product(models.Model):
     title = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
     description = models.CharField(max_length=200)
-    link = models.CharField(max_length=200)
     # date = models.DateTimeField('date published')
     # image = models.ImageField(default='default.jpg', upload_to='product_pics')
 
--- main/views.py
+++ main/views.py
@@ -4,15 +4,12 @@ from django.core import serializers
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import get_object_or_404,render
+from django.shortcuts import get_object_or_404, render
 
 from django.http import Http404, HttpResponseRedirect
 from django.http import HttpResponse
 from django.views import generic
 
-# class IndeView(generic.DetailView):
-#     template_name = 'index.html'
-#     # context_object_name = 'value'
 from auction.models import Product
 
 
@@ -20,13 +17,13 @@ def index(request):
     latest_product_list = Product.objects.all()
     # context = {'val': "Nuri you are pidor",'latest_product_list':latest_product_list}
 
-    val = serializers.serialize("json", latest_product_list)
-    # print("ADADADDA"+str(latest_product_list))
-    # new_list = []
-    # for x in val:
-    #     print(x)
-    #     # new_list.append(x['fields'])
-
-    print("ADADADDA"+str(val))
-    context = {'items_json': val}
+    # ! Important: Use {{ ... |safe}} to prevent character escape in the template
+    context = {
+        'trending_products': list(map(lambda x: {
+            "title": x['title'],
+            "price": x['price'],
+            "description": x['description'],
+            "link": f"/product/{x['id']}"
+        }, list(latest_product_list.values())))
+    }
     return render(request, 'main/index.html', context)
