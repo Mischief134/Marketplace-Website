3068608075b98ed4b7b60ed6a95188f5421b1b87
Nuri <nuri.amiraslan@mail.mcgill.ca>
2020-04-19 16:55:54 -0400

    Added password recovery

--- COMP307FinalProject/settings.py
+++ COMP307FinalProject/settings.py
@@ -143,6 +143,13 @@ CHANNEL_LAYERS = {
     },
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '307projectgroup@gmail.com'
+EMAIL_HOST_PASSWORD = 'peysarpeysarov666'
+
 # if DEBUG:
 #     STRIPE_PUBLISHABLE_KEY=''
 #     STRIPE_SECRET_KEY=''
